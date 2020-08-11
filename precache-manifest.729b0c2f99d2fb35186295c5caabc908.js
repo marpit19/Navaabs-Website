572e24263c22750158ba993a3787f22f16e5c83d
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2020-08-11 21:32:24 +0530

    Updates

---
+++ precache-manifest.729b0c2f99d2fb35186295c5caabc908.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6079e97a66eb1dc50f20afbcb6bc23",
+    "url": "/Navaabs-Website/index.html"
+  },
+  {
+    "revision": "e128e711c3b122bb4791",
+    "url": "/Navaabs-Website/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "b12a4ca5528f1c6d1e6d",
+    "url": "/Navaabs-Website/static/css/main.ddb7d47b.chunk.css"
+  },
+  {
+    "revision": "e128e711c3b122bb4791",
+    "url": "/Navaabs-Website/static/js/2.0f9d7087.chunk.js"
+  },
+  {
+    "revision": "ac02e5d8661105c8db3762573eb0e69f",
+    "url": "/Navaabs-Website/static/js/2.0f9d7087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12a4ca5528f1c6d1e6d",
+    "url": "/Navaabs-Website/static/js/main.ca90bf54.chunk.js"
+  },
+  {
+    "revision": "f410c79fa2e97fca40a8",
+    "url": "/Navaabs-Website/static/js/runtime-main.c5a27726.js"
+  },
+  {
+    "revision": "36e0b7b5ecdfbeb88d20e883bfc59766",
+    "url": "/Navaabs-Website/static/media/1.36e0b7b5.jpg"
+  },
+  {
+    "revision": "97c2e004fbd6ac171d8300081d5b2949",
+    "url": "/Navaabs-Website/static/media/10.97c2e004.jpg"
+  },
+  {
+    "revision": "44d57c7b0b167f8d765436657ca07542",
+    "url": "/Navaabs-Website/static/media/11.44d57c7b.jpg"
+  },
+  {
+    "revision": "dd7f032b188689036a5ad622c61a2478",
+    "url": "/Navaabs-Website/static/media/12.dd7f032b.jpg"
+  },
+  {
+    "revision": "fd7423dc9f3944d6b6b059d543768de7",
+    "url": "/Navaabs-Website/static/media/4.fd7423dc.jpg"
+  },
+  {
+    "revision": "f3959ac9c546334ea67ffc8b557ab9f7",
+    "url": "/Navaabs-Website/static/media/banner1.f3959ac9.jpg"
+  },
+  {
+    "revision": "40952877d179819ded8ebc3d48e48435",
+    "url": "/Navaabs-Website/static/media/banner2.40952877.jpg"
+  },
+  {
+    "revision": "a1a9c5a09346a56947ff1cf3094578e8",
+    "url": "/Navaabs-Website/static/media/banner3.a1a9c5a0.jpg"
+  },
+  {
+    "revision": "981f511521727d731e19ffccb906e399",
+    "url": "/Navaabs-Website/static/media/banner6.981f5115.jpg"
+  },
+  {
+    "revision": "b8d45600a1ce64130caf5bc47cc456e1",
+    "url": "/Navaabs-Website/static/media/banner7.b8d45600.jpg"
+  },
+  {
+    "revision": "66bf894b5d318e724700b4963982118a",
+    "url": "/Navaabs-Website/static/media/banner8.66bf894b.jpg"
+  }
+]);
